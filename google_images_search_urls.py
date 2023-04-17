0a346374a6576b6a8890a9226575acab636e5bc6
Thotakuri Ravi Kumar <78135736+thotakuriravi@users.noreply.github.com>
2023-04-17 11:42:11 +0530

    add try block for skip errors

--- google_images_search_urls.py
+++ google_images_search_urls.py
@@ -10,18 +10,26 @@ google_images_url = f"https://www.google.com/search?q={search_term}&tbm=isch"
 driver.get(google_images_url)
 
 # find all the image divs and extract their links
-image_divs = driver.find_elements(By.CLASS_NAME,'isv-r')
+image_divs = driver.find_elements( By.CLASS_NAME,'isv-r')
 
+i = 0
 for image_div in image_divs[1:]:
-    img_tag = image_div.find_element(By.TAG_NAME, 'img')
-    img_tag.click()
-    time.sleep(3)
-    
-    # Find the div tag that appears with class name 'iPVvYb' after clicking the image
-    final_image_div = driver.find_element(By.CLASS_NAME, 'iPVvYb')
-    if final_image_div:
-        print(final_image_div.get_attribute('src'))
+    try:
+        img_tag = image_div.find_element(By.TAG_NAME, 'img')
+        img_tag.click()
+        time.sleep(1)
         
-    time.sleep(3)
+        # Find the div tag that appears with class name 'iPVvYb' after clicking the image
+        final_image_div = driver.find_elements(By.CLASS_NAME, 'iPVvYb')[0]
+        if final_image_div:
+            url = final_image_div.get_attribute('src')
+            response = requests.get(url, stream=True)
+            with open(f"images/{promt}_{i+1}.jpg", "wb") as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+                i = i+1
+            del response
+    except:
+        continue
+    # time.sleep(1)
 
 driver.quit() 
